6802605a4c4fe5dd05d2228d4c5cd87d9c9117ff
IandRover <0102030405jacky@gmail.com>
2021-06-03 14:29:14 +0800

    Add files

---
+++ omniglot_memorization_/torchmeta/modules/module.py
@@ -0,0 +1,55 @@
+import torch
+import torch.nn as nn
+import re
+
+from collections import OrderedDict
+
+
+class MetaModule(nn.Module):
+    """
+    Base class for PyTorch meta-learning modules. These modules accept an
+    additional argument `params` in their `forward` method.
+
+    Notes
+    -----
+    Objects inherited from `MetaModule` are fully compatible with PyTorch
+    modules from `torch.nn.Module`. The argument `params` is a dictionary of
+    tensors, with full support of the computation graph (for differentiation).
+    """
+    def __init__(self):
+        super(MetaModule, self).__init__()
+        self._children_modules_parameters_cache = dict()
+
+    def meta_named_parameters(self, prefix='', recurse=True):
+        gen = self._named_members(
+            lambda module: module._parameters.items()
+            if isinstance(module, MetaModule) else [],
+            prefix=prefix, recurse=recurse)
+        for elem in gen:
+            yield elem
+
+    def meta_parameters(self, recurse=True):
+        for name, param in self.meta_named_parameters(recurse=recurse):
+            yield param
+
+    def get_subdict(self, params, key=None):
+        if params is None:
+            return None
+
+        all_names = tuple(params.keys())
+        if (key, all_names) not in self._children_modules_parameters_cache:
+            if key is None:
+                self._children_modules_parameters_cache[(key, all_names)] = all_names
+
+            key_escape = re.escape(key)
+            key_re = re.compile(r'^{0}\.(.+)'.format(key_escape))
+            # Compatibility with DataParallel
+            if not any(filter(key_re.match, all_names)):
+                key_re = re.compile(r'^module\.{0}\.(.+)'.format(key_escape))
+
+            self._children_modules_parameters_cache[(key, all_names)] = [
+                key_re.sub(r'\1', k) for k in all_names if key_re.match(k) is not None]
+
+        names = self._children_modules_parameters_cache[(key, all_names)]
+
+        return OrderedDict([(name, params[f'{key}.{name}']) for name in names])
